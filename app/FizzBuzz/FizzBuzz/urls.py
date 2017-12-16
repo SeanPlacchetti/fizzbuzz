4947477f62c98389f2273970d5f73a2ab2d4448c
Sean Placchetti <Sean.P.Placchetti@gmail.com>
2017-12-16 15:16:14 -0500

    Fix urls.py

--- app/FizzBuzz/FizzBuzz/urls.py
+++ app/FizzBuzz/FizzBuzz/urls.py
@@ -5,15 +5,17 @@ from rest_framework import routers
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
-from FizzBuzz.views import FizzBuzzViewSet, UserViewSet
+from FizzBuzzApp.views import FizzBuzzViewSet, UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'fizzbuzz', FizzBuzzViewSet)
 router.register(r'users', UserViewSet)
 
-urlpatterns = [
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include(router.urls)),
-]
+urlpatterns = [url(r'^', include(router.urls)), ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns.append(url(r'^api/', include('rest_framework.urls',
+                                             namespace='rest_framework')))
+    urlpatterns.append(url(r'^admin/', admin.site.urls))
